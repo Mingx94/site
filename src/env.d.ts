bb7bea9d7fec11a6d02df1f7220e4b7c752a2405
Michael Tsai <mingxcv@gmail.com>
2025-06-23 10:29:06 +0800

    feat: update social order

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,15 @@ declare global {
   interface Window {
     toggleFont: (font: string) => void;
   }
+
+  interface Array<T> {
+    filter<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S): S[];
+  }
+
+  interface ObjectConstructor {
+    keys<T>(o: T): (keyof T)[];
+  }
 }
+
 
 export {};
