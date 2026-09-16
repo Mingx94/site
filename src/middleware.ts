e26ad0e0c7424fa690958361c2505de70bc5178f
Michael Tsai <mingxcv@gmail.com>
2026-09-16 14:52:24 +0800

    Use Google Fonts and add admin shortcut

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,8 +6,8 @@ import { prefersMarkdown } from "./lib/accept";
 const CSP = [
   "default-src 'self'",
   "script-src 'self' 'unsafe-inline' https://static.cloudflareinsights.com https://challenges.cloudflare.com",
-  "style-src 'self' 'unsafe-inline'",
-  "font-src 'self' data:",
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+  "font-src 'self' data: https://fonts.gstatic.com",
   "img-src 'self' data: blob:",
   "connect-src 'self' https://cloudflareinsights.com https://challenges.cloudflare.com",
   "frame-src 'self' https://challenges.cloudflare.com",
